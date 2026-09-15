a748bd7380015d695874fdefb89bb4f840f87259
aasthab2245-sketch <aasthab2245@gmail.com>
2026-09-15 19:59:41 +0530

    Add files via upload

---
+++ 10. conversion of time.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int seconds, hours, minutes, remainingSeconds;
+
+    printf("Enter time in seconds: ");
+    scanf("%d", &seconds);
+
+    hours = seconds / 3600;
+    remainingSeconds = seconds % 3600;
+    minutes = remainingSeconds / 60;
+    remainingSeconds = remainingSeconds % 60;
+
+    printf("Time = %02d:%02d:%02d\n", hours, minutes, remainingSeconds);
+
+    return 0;
+}
